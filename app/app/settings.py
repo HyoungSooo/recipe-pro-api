d3d1dcb6878b5603358e94c0675a59fd62c11184
Hyoungsooo <aaa57403@gmail.com>
2022-06-04 00:56:30 +0900

    apply drf_spectacular to see our api

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'rest_framework',
+    'drf_spectacular',
 
 ]
 
@@ -132,3 +134,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'core.User'
+
+
+# REST Framework
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
