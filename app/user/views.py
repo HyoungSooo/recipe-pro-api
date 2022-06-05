e3f02fe1bcf9e6703ca10925a7397e97c5831ded
Hyoungsooo <aaa57403@gmail.com>
2022-06-05 18:03:34 +0900

    test retry

--- app/user/views.py
+++ app/user/views.py
@@ -2,7 +2,7 @@
 Views for the user app.
 """
 
-from rest_framework import generics, permissions, authentication, viewsets
+from rest_framework import generics, permissions, authentication
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
